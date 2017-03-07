279ba1d2694a65246fe31efeed9f0f0ace161644
nico <nico@localhost>
2017-03-07 09:41:41 +0100

    initial commit of multithreaded_server package

---
+++ src/multithreaded_server/Demo.java
@@ -0,0 +1,17 @@
+package multithreaded_server;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Server server = new Server(9000);
+		new Thread(server).start();
+
+		try {
+		    Thread.sleep(1000 * 1000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();  
+		}
+		System.out.println("Stopping Server");
+		server.stop();
+	}
+}
---
+++ src/multithreaded_server/WorkerThread.java
@@ -0,0 +1,35 @@
+package multithreaded_server;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class WorkerThread implements Runnable{
+
+    protected Socket clientSocket = null;
+    protected String serverText   = null;
+
+    public WorkerThread(Socket clientSocket, String serverText) {
+        this.clientSocket = clientSocket;
+        this.serverText   = serverText;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream input  = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+            long time = System.currentTimeMillis();
+            output.write(("HTTP/1.1 200 OK\n\nWorkerThread: " +
+            			this.serverText + " - " +
+            			time + "").getBytes());
+            output.close();
+            input.close();
+            System.out.println("Request processed: " + time);
+        } catch (IOException e) {
+            //report exception somewhere.
+            e.printStackTrace();
+        }
+    }
+}
