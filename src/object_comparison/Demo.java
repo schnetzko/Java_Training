c1de888c6ad24423ad0360dd0021494ec3293cf2
nico <nico@vuelta>
2018-05-01 11:10:30 +0200

    refactored print_hashCodes(..) using Java Generics, added more comments

--- src/object_comparison/Demo.java
+++ src/object_comparison/Demo.java
@@ -10,16 +10,17 @@ import java.util.Set;
 public class Demo {
 
 	/**
-	 * Prints hash-codes to System.out from objects containing in the given arrays of the primitives data types
-	 * and from objects in the given array of Person class. 
+	 * Prints hash-codes to System.out from objects containing in the given array (supports all non-primitive data types).
 	 * 
-	 * @param short_array
-	 * @param long_array
-	 * @param persons
+	 * "System.identityHashCode(Object obj)" prints the hash-code of an object using the original hashCode implementation (Object.hashCode()).
+	 * "obj.hashCode(Object obj)" prints the hash-code of an object using its hashCode() implementation if it was overwritten. 
+	 * Otherwise it will call Object.hashCode().
+	 * 
+	 * @param array of any type
 	 */
-	public static void print_hashCodes(short [] short_array, long [] long_array, Person persons[]){
+	public static <T> void print_hashCodes(T [] array){
 		
-		System.out.println("print_hashCodes(..)");
+		System.out.println("print_hashCodes(..) of type " + array.getClass().getTypeName() + " using Java Generics");
 		/* Note:	There is no hashCode() method for primitives available,
 	     * 			because primitives are not classes. Thus, they can't inherit 
 	     * 			from class Object the base-implementation of hashCode() to call it.
@@ -37,26 +38,56 @@ public class Demo {
 	     * 			In more detail: Each different hash-code calculated by the default
 	     * 			hashCode() method points to a bucket. All objects with the same hash-code
 	     * 			are stored in the same bucket. */	
+		for (T element : array) {
+			System.out.println("\tSystem.identityHashCode(" + element + "): " + System.identityHashCode(element));
+			System.out.println("\tperson.hashCode(): " + element.hashCode());
+			System.out.println();
+		}
+			System.out.println();
+	}
+	
+	/**
+	 * Prints hash-codes to System.out from values containing in the given arrays of the primitives data types.
+	 * (Java Generics do not support primitives. An alternative could be to wrap the primitives in their corresponding wrapper types
+	 * like Short, Long,... Then calling "<Type t> void print_hashCodes(T[])" would work.) 
+	 * 
+	 * @param short_array
+	 * @param long_array
+	 */
+	public static void print_hashCodes(short [] short_array, long [] long_array){
+		
+		System.out.println("print_hashCodes(..) for primitive data types");
+		/* Note:	There is no hashCode() method for primitives available,
+	     * 			because primitives are not classes. Thus, they can't inherit 
+	     * 			from class Object the base-implementation of hashCode() to call it.
+	     * 			But to still call the base-implementation of hashCode for primitives,
+	     * 			just call System.identityHashCode() as an alternative.
+	     * 
+	     * 			The default implementation of a hash code for a variable or object
+	     * 			returns a memory address MOD by integer max value (= 2^31-1).
+	     * 			That means 2 different variables or objects can have the same hash-code,
+	     * 			but are not identical. And that means the hash-code is not an unique id
+	     * 			(reference or memory address) for an object.
+	     * 			
+	     * 			If 2 different objects have the same hash code, it's just a hash-collision.
+	     * 			In this case these 2 objects will by compared by equality afterwards. 
+	     * 			In more detail: Each different hash-code calculated by the default
+	     * 			hashCode() method points to a bucket. All objects with the same hash-code
+	     * 			are stored in the same bucket. */	
+		System.out.println("\ttype short[]");
 		for (short element : short_array) {
 			System.out.print("\tSystem.identityHashCode(" + element + "): ");
 			System.out.print(System.identityHashCode(element));
 			System.out.println();
 		}
 		System.out.println();
-		
+		System.out.println("\ttype long[]");
 		for (long element : long_array) {
 			System.out.print("\tSystem.identityHashCode(" + element + "): ");
 			System.out.print(System.identityHashCode(element));
 			System.out.println();
 		}
-		System.out.println();
-		
-		for (Person person : persons) {
-			System.out.println("\t" + person);			
-			System.out.println("\tSystem.identityHashCode(person): " + System.identityHashCode(person));
-			System.out.println("\tperson.hashCode(): " + person.hashCode());
-			System.out.println();
-		}
+		System.out.println();		
 	}
 	
 	/**
@@ -64,53 +95,53 @@ public class Demo {
 	 * to avoid duplicate code, but unfortunately the printed hash-codes are not the same like
 	 * primitives_hashCode() with redundant code.
 	 */	
-	public static void print_hashCodes(Object obj){
-		
-		System.out.println("print_hashCodes(..) using Java reflection");
-
-		// iterate over all class members
-		for (Field field : obj.getClass().getDeclaredFields()) {
-//			System.out.println("field is of a primitive type?: " + field.getType().isPrimitive() + "\n");
-//			System.out.println("field is an array type?: " + field.getType().isArray() + "\n");
-//			System.out.println("field-type: " + field.getType() + "\n");
-//			System.out.println("field-name: " + field.getName() + "\n");
-//			System.out.println("field-component type: " + field.getType().getComponentType() + "\n");
-			if ((field.getType().getComponentType() != null) && /* check if it's an array */
-				(field.getType().getComponentType().isPrimitive()) /* check if it's an array of a primitive type */ 
-				||
-				(field.getType().isArray()) &&
-				(!field.getType().isPrimitive())) {
-				Object array = null;
-				try {
-					array = field.get(obj); // conversion to an array with one dimension
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				}
-				if (array != null) {
-					int length = Array.getLength(array);
-					for (int i = 0; i < length; i++) {
-						System.out.print("\tSystem.identityHashCode(" + Array.get(array, i) + "): ");
-						System.out.print(System.identityHashCode(Array.get(array, i)) + "\n");
-					}				
-				}
-			}			
-			else {
-				System.out.println("Data type not supported!");
-			}
-				
+//	public static void print_hashCodes(Object obj){
+//		
+//		System.out.println("print_hashCodes(..) using Java reflection");
+//
+//		// iterate over all class members
+//		for (Field field : obj.getClass().getDeclaredFields()) {
+////			System.out.println("field is of a primitive type?: " + field.getType().isPrimitive() + "\n");
+////			System.out.println("field is an array type?: " + field.getType().isArray() + "\n");
+////			System.out.println("field-type: " + field.getType() + "\n");
+////			System.out.println("field-name: " + field.getName() + "\n");
+////			System.out.println("field-component type: " + field.getType().getComponentType() + "\n");
+//			if ((field.getType().getComponentType() != null) && /* check if it's an array */
+//				(field.getType().getComponentType().isPrimitive()) /* check if it's an array of a primitive type */ 
+//				||
+//				(field.getType().isArray()) &&
+//				(!field.getType().isPrimitive())) {
+//				Object array = null;
 //				try {
-//					out.append("field-value: " + field.get(obj) + "\n");
+//					array = field.get(obj); // conversion to an array with one dimension
 //				} catch (IllegalArgumentException e) {
 //					e.printStackTrace();
 //				} catch (IllegalAccessException e) {
 //					e.printStackTrace();
 //				}
-
-			System.out.println();
-		}
-	}	
+//				if (array != null) {
+//					int length = Array.getLength(array);
+//					for (int i = 0; i < length; i++) {
+//						System.out.print("\tSystem.identityHashCode(" + Array.get(array, i) + "): ");
+//						System.out.print(System.identityHashCode(Array.get(array, i)) + "\n");
+//					}				
+//				}
+//			}			
+//			else {
+//				System.out.println("Data type not supported!");
+//			}
+//				
+////				try {
+////					out.append("field-value: " + field.get(obj) + "\n");
+////				} catch (IllegalArgumentException e) {
+////					e.printStackTrace();
+////				} catch (IllegalAccessException e) {
+////					e.printStackTrace();
+////				}
+//
+//			System.out.println();
+//		}
+//	}	
 
 	/**
 	 * Prints results about comparison between values of primitive arrays
@@ -177,9 +208,11 @@ public class Demo {
 	
 	public static void main(String[] args) {
 
-		short [] primitives_short = {55, 55, 3};
+		short [] short_primitives = {55, 55, 3};
+		Short [] short_wrapper = {55, 55, 3};
 		
-		long [] primitives_long = {55325, 55325, 3685854};
+		long [] long_primitives = {55325, 55325, 3685854};
+		Long [] long_wrapper = {(long)55325, (long)55325, (long) 368988854};
 
 		Person p1 = new Person (50, "Heinz");
 	    Person p2 = new Person (45, "Regina");
@@ -190,8 +223,11 @@ public class Demo {
 //		DataTypes dataTypes = new DataTypes(primitives_short);
 //		Demo.print_hashCodes(dataTypes);
 		
-		Demo.print_hashCodes(primitives_short, primitives_long, persons);
-		Demo.print_comparisons(primitives_short, primitives_long, persons);
+		Demo.print_hashCodes(short_primitives, long_primitives);
+		Demo.print_hashCodes(short_wrapper);
+		Demo.print_hashCodes(long_wrapper);
+		Demo.print_hashCodes(persons);
+//		Demo.print_comparisons(primitives_short, primitives_long, persons);
 
 		System.out.println();
 		System.out.println("ArrayList (non-hash-based) calls equals() of Person class");
