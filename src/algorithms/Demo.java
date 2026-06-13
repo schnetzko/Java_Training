6612eb5850554347ece66ddbea8667d03a6dd2d4
Schnetzko <schnetzko@schnetzko.de>
2026-06-13 23:39:53 +0200

    cleanup exception handling, improve CLI output

--- src/algorithms/Demo.java
+++ src/algorithms/Demo.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Demo implements HandleLists {
+public class Demo {
 	public static void main(String[] args) {
 		StringBuffer outputStr = new StringBuffer();
 		outputStr.append("Sorting of unsorted integer lists\n");
@@ -15,8 +15,8 @@ public class Demo implements HandleLists {
 		outputStr.append("Input: multiple lists of unsorted integers\n");
 		outputStr.append("Output: one sorted integer list\n");
 		outputStr.append("\n");
-		outputStr.append("(n)umber of integer lists you want to sort\n");
-		outputStr.append("(e)xit to exit\n");
+		outputStr.append("type (s)orting of integer lists\n");
+		outputStr.append("type (e)xit to exit\n");
 		
 		String inputStr = null;
 
@@ -39,7 +39,7 @@ public class Demo implements HandleLists {
 				break;
 			}
 			// (n)umber of arrays handling
-			else if(inputStr.equals("n")){
+			else if(inputStr.equals("s")){
 				handleLists(br, outputStr);
 			}
 			else {
@@ -88,10 +88,9 @@ public class Demo implements HandleLists {
 		}
 		else {
 			ArrayList<List<Integer>> arrayLists = new ArrayList<List<Integer>>(numberOfarrayLists);
-			boolean restart = false;
 			int numberOfelements = 0;
 
-			for (int i = 0; i < numberOfarrayLists && !restart; i++){
+			for (int i = 0; i < numberOfarrayLists; i++){
 				System.out.print("enter number of elements for list " + i + ":");
 				// number of list validation
 				try {
@@ -101,30 +100,24 @@ public class Demo implements HandleLists {
 				catch (IOException e){
 					System.out.println("Could not read data from command line, try it again.");
 					System.out.print(outputStr.toString()); 
-					arrayLists.clear();
-					restart = true;
-					break;
+					return;
 				}
 				catch (NumberFormatException e){
 					System.out.println("Incorrect number format, try it again.");
 					System.out.print(outputStr.toString()); 
-					arrayLists.clear();
-					restart = true;
-					break;
+					return;
 				}
 				if (numberOfelements <= 0){
 					System.out.println("please enter a number > 0");
 					System.out.print(outputStr.toString());
-					arrayLists.clear();
-					restart = true;
-					break;
+					return;
 				}
 				// empty array list created
 				arrayLists.add(Arrays.asList(new Integer [Integer.parseInt(inputStr)]));
 
 				for (int j = 0; j < arrayLists.get(i).size(); j++) {
 					System.out.println("enter integer on index [" + j + "]: ");
-					// Validation
+					// validation of each element of list
 					try {
 						inputStr = br.readLine();
 						arrayLists.get(i).set(j, Integer.parseInt(inputStr));
@@ -132,57 +125,49 @@ public class Demo implements HandleLists {
 					catch (IOException e){
 						System.out.println("Could not read data from command line, try it again.");
 						System.out.print(outputStr.toString());
-						arrayLists.clear();
-						restart = true;
-						break;
+						return;
 					}
 					catch (NumberFormatException e){
 						System.out.println("Incorrect number format, try it again.");
 						System.out.print(outputStr.toString());
-						arrayLists.clear();
-						restart = true;
-						break;
+						return;
 					}
 				}
-				if (!restart){
-					System.out.print("created list with " + arrayLists.get(i).size() + " elements:");
-					System.out.print(arrayLists.get(i));
-					System.out.println();
-				}
+				System.out.print("created list with " + arrayLists.get(i).size() + " elements:");
+				System.out.print(arrayLists.get(i));
+				System.out.println();
 			}	
-			if (!restart){
+			// apply sorting algorithms on the lists and print the result
+			System.out.println();
+			System.out.println("1. start sorting each list...");
+			System.out.println();
+			for (int i = 0; i < arrayLists.size(); i++){
+				//	Sorting.quick_sort(arrayLists.get(i), 0, arrayLists.get(i).size()-1);
+				Sorting.insertion_sort(arrayLists.get(i));
+				System.out.print("list " + i + " sorted:");
+				System.out.print(arrayLists.get(i));
 				System.out.println();
-				System.out.println("1. start sorting each list...");
+			}		
+	
+			arrayListResult = arrayLists.get(0);
+			
+			if (arrayLists.size() > 1){
 				System.out.println();
-				for (int i = 0; i < arrayLists.size(); i++){
-					//	Sorting.quick_sort(arrayLists.get(i), 0, arrayLists.get(i).size()-1);
-					Sorting.insertion_sort(arrayLists.get(i));
-					System.out.print("array list " + i + " sorted:");
-					System.out.print(arrayLists.get(i));
-					System.out.println();
-				}		
-		
-				arrayListResult = arrayLists.get(0);
-				
-				if (arrayLists.size() > 1){
-					System.out.println();
-					System.out.println("2. start sorting each list with each other...");
-					System.out.println();
-					for (int i = 1; i < arrayLists.size(); i++){
-						arrayListResult = Sorting.sort(arrayListResult, arrayLists.get(i));
-					}
-					System.out.println("resulting sorted list: ");
-					System.out.println(arrayListResult);
-					System.out.println();
-					System.out.print(outputStr.toString());
+				System.out.println("2. start sorting each list with each other...");
+				System.out.println();
+				for (int i = 1; i < arrayLists.size(); i++){
+					arrayListResult = Sorting.sort(arrayListResult, arrayLists.get(i));
 				}
-				else{
-					System.out.println();
-					System.out.println("there is only 1 list which is already sorted...");
-					System.out.println();	
-				}	
-				arrayLists.clear();
+				System.out.println("resulting sorted list: ");
+				System.out.println(arrayListResult);
+				System.out.println();
+				System.out.print(outputStr.toString());
 			}
+			else{
+				System.out.println();
+				System.out.println("there is only 1 list which is already sorted...");
+				System.out.println();	
+			}	
 		}
 	}
 }
